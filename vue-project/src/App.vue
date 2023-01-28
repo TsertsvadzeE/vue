01ec12f55e1864fe9bf01adf5c5bdf4b169eeb77
TsertsvadzeE <eliso.tsertsvadze99@gmail.com>
2023-01-28 19:56:38 +0400

    lesson 7

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import listMenu from './listMenu.vue';
+export default{
+  components:{
+    listMenu
+  },
+  data(){
+    return{
+      data: [
+        {
+          id: 1,
+          title: "Home"
+        },
+        {
+          id: 2,
+          title: "TOS"
+        },
+        {
+          id: 3,
+          title: "About Us"
+        },
+        {
+          id: 4,
+          title: "Contact"
+        },
+      ]
+    }
+  }
+}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <listMenu :menuItems="data"/>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ vue-project/src/listMenu.vue
@@ -0,0 +1,21 @@
+<script>
+export default{
+    props:{
+        menuItems: {type:Array, required:true}
+    }
+}
+</script>
+<template>
+    <ul>
+        <li v-for="item in menuItems" :key="item.id">{{ item.title }}</li>
+    </ul>
+</template>
+<style scoped>
+ul {
+    display: flex;
+}
+ul li{
+    list-style: none;
+    margin-right: 25px;
+}
+</style>
